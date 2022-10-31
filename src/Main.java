c00c30ab516f7cf5404e90b94cc27630dad1b6e7
Fabrício Vaz <fabricio.vaz2004@gmail.com>
2022-10-31 16:41:49 -0300

    Projetada uma simulação de uma classe Endereco.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Endereco cliente1 = new Endereco("30664782");
+        System.out.println(cliente1.getCep());
+
+    }
+}
